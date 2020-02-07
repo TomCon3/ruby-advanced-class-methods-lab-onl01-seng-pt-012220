5b7d7da9a10977ed01608b6a0ba4b841a7c37f45
Tommy Connor <tconnor98@aol.com>
2020-02-07 02:43:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,12 @@ class Song
   
   def self.find_or_create_by_name(title)
     result = self.find_by_name
-    if 
+    if result
+      result
+    else
+      self.create_by_name
+    end
+  end
     
   
   
